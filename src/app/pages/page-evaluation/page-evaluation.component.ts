8eb60f784833ef22383ac855d345af29ff794a8f
Daniel Habenicht <daniel-habenicht@outlook.de>
2019-04-13 15:02:34 +0200

    Update evaluation

--- src/app/pages/page-evaluation/page-evaluation.component.ts
+++ src/app/pages/page-evaluation/page-evaluation.component.ts
@@ -18,28 +18,31 @@ export class PageEvaluationComponent implements OnInit {
         }
       ];
       this.videoUrl = data.videoUrl;
+      this.wordMapArray = data.evaluation.wordCount;
+      this.totalTime = data.evaluation.time;
+      this.totalWords = data.evaluation.words;
     });
   }
 
   @HostBinding('class')
   class = 'CenteredPage';
 
+  totalWords = 0;
+  totalTime = '';
   videoUrl = '';
   sentimentArray: any[] = multi;
   emotionArray: any[] = multi;
   wordMapArray: any[] = wordMap;
 
-  view: any[] = [1800, 200];
+  view: any[] = [1000, 200];
 
   // options
   showXAxis = true;
   showYAxis = true;
   gradient = false;
   showLegend = false;
-  showXAxisLabel = true;
-  xAxisLabel = 'Timeline';
-  showYAxisLabel = true;
-  yAxisLabel = 'Sentiment';
+  showXAxisLabel = false;
+  showYAxisLabel = false;
 
   colorScheme = {
     domain: ['#ff1744', '#d500f9', '#2979ff', '#00e5ff', '#c6ff00', '#ff3d00']
--- src/app/state/AppState.ts
+++ src/app/state/AppState.ts
@@ -14,12 +14,7 @@ export class SetProgress {
 @State<AppStateModel>({
   name: 'app',
   defaults: {
-    evaluation: {
-      sentiments: [],
-      time: '',
-      words: 0,
-      wordCount: []
-    },
+    evaluation: getDefaultEvaluation(),
     progress: -1,
     uploaded: false,
     analyzed: false,
@@ -43,104 +38,15 @@ export class AppState {
       data => {
         ctx.patchState({
           evaluation: data,
-          analyzed: true
+          analyzed: true,
+          videoUrl: action.videoUrl
         });
       },
       error => {
         ctx.patchState({
           analyzed: true,
-          evaluation: {
-            time: '0:00:14.614',
-            words: 20,
-            sentiments: [
-              {
-                name: 'Neutral',
-                value: 0.6295
-              },
-              {
-                name: 'Positive',
-                value: 0.3695
-              }
-            ],
-            wordCount: [
-              {
-                name: 'this',
-                value: 2
-              },
-              {
-                name: 'for',
-                value: 2
-              },
-              {
-                name: 'hello',
-                value: 1
-              },
-              {
-                name: 'is',
-                value: 1
-              },
-              {
-                name: 'my',
-                value: 1
-              },
-              {
-                name: 'radio',
-                value: 1
-              },
-              {
-                name: 'subject',
-                value: 1
-              },
-              {
-                name: 'i',
-                value: 1
-              },
-              {
-                name: 'm',
-                value: 1
-              },
-              {
-                name: 'a',
-                value: 1
-              },
-              {
-                name: 'very',
-                value: 1
-              },
-              {
-                name: 'good',
-                value: 1
-              },
-              {
-                name: 'speaker',
-                value: 1
-              },
-              {
-                name: 'an',
-                value: 1
-              },
-              {
-                name: 'thank',
-                value: 1
-              },
-              {
-                name: 'you',
-                value: 1
-              },
-              {
-                name: 'listening',
-                value: 1
-              },
-              {
-                name: 'um',
-                value: 1
-              },
-              {
-                name: 'goodbye',
-                value: 1
-              }
-            ]
-          }
+          evaluation: getDefaultEvaluation(),
+          videoUrl: action.videoUrl
         });
       }
     );
@@ -160,4 +66,99 @@ export interface AppStateModel {
   uploaded: boolean;
   analyzed: boolean;
   videoUrl: string;
+}
+
+function getDefaultEvaluation() {
+  return {
+    time: '0:00:14.614',
+    words: 20,
+    sentiments: [
+      {
+        name: 'Neutral',
+        value: 0.6295
+      },
+      {
+        name: 'Positive',
+        value: 0.3695
+      }
+    ],
+    wordCount: [
+      {
+        name: 'this',
+        value: 2
+      },
+      {
+        name: 'for',
+        value: 2
+      },
+      {
+        name: 'hello',
+        value: 1
+      },
+      {
+        name: 'is',
+        value: 1
+      },
+      {
+        name: 'my',
+        value: 1
+      },
+      {
+        name: 'radio',
+        value: 1
+      },
+      {
+        name: 'subject',
+        value: 1
+      },
+      {
+        name: 'i',
+        value: 1
+      },
+      {
+        name: 'm',
+        value: 1
+      },
+      {
+        name: 'a',
+        value: 1
+      },
+      {
+        name: 'very',
+        value: 1
+      },
+      {
+        name: 'good',
+        value: 1
+      },
+      {
+        name: 'speaker',
+        value: 1
+      },
+      {
+        name: 'an',
+        value: 1
+      },
+      {
+        name: 'thank',
+        value: 1
+      },
+      {
+        name: 'you',
+        value: 1
+      },
+      {
+        name: 'listening',
+        value: 1
+      },
+      {
+        name: 'um',
+        value: 1
+      },
+      {
+        name: 'goodbye',
+        value: 1
+      }
+    ]
+  };
 }
