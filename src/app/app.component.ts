7bcf14a59af363e7805a33055c9d71c488621275
Daniel Habenicht <daniel-habenicht@outlook.de>
2019-04-13 04:03:15 +0200

    Add Angular material and setup blob storage upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { BlobService, UploadConfig, UploadParams } from 'angular-azure-blob-service';
+
+const Config: UploadParams = {
+  sas:
+    '?sv=2018-03-28&ss=bfqt&srt=sco&sp=rwdlacup&st=2019-04-12T01%3A03%3A00Z&se=2019-04-15T01%3A03%3A00Z&sig=%2B%2Bwg8RJ0FYz4HP4OICOBVbJcz4kzG8MdD8BNqSsqEMk%3D',
+  storageAccount: 'ahhhmalizerstorage',
+  containerName: 'test'
+};
 
 @Component({
   selector: 'app-root',
@@ -7,4 +15,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Ahhhmalizer';
+  progress = 0;
+
+  constructor(private blob: BlobService) {}
+  upload(file: File) {
+    if (file !== null) {
+      const baseUrl = this.blob.generateBlobUrl(Config, file.name);
+      const config = {
+        baseUrl,
+        sasToken: Config.sas,
+        blockSize: 1024 * 64, // OPTIONAL, default value is 1024 * 32
+        file,
+        complete: () => {
+          console.log('Transfer completed !');
+        },
+        error: err => {
+          console.log('Error:', err);
+        },
+        progress: percent => {
+          this.progress = percent;
+        }
+      };
+      this.blob.upload(config);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlobModule } from 'angular-azure-blob-service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from 'src/app/modules/material/material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, MaterialModule, BlobModule.forRoot(), BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
